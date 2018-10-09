f8105c15abc77c14ef9545ebf56d027a9bc75033
Zac Hokanson <zac.hokanson@gmail.com>
2018-10-09 11:00:14 -0600

    Added Bootstrap

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+var charName = document.querySelector("#characterName");
+
 fetch('https://swapi.co/api/people/1/')
 .then(function(response) {
   return response.json();
 })
 .then(function(myJson) {
-  console.log(JSON.stringify(myJson));
+    charName.textContent = myJson.name;
+  console.log(myJson.name);
 });
