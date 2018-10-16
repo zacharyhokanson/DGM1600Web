4ea48e8ef517313c5cb6c27ee1cc6e5b6b71d04f
Unknown <zac.hokanson@gmail.com>
2018-10-16 10:44:30 -0600

    added import

--- main.js
+++ main.js
@@ -1,41 +1,5 @@
-let charName = document.querySelector("#characterName");
-// var hairColor = document.querySelector("#hairColor");
-// var eyeColor = document.querySelector("#eyeColor");
+import {films} from 'assets/films.js';
 
-// fetch('https://swapi.co/api/films/')
-// .then(function(response) {
-//   return response.json();
-// })
-// .then(function(myJson) {
-//   // myJson.results.forEach(function(element){
-//   //   console.log(element);
-//   // });
+let filmList =document.queryCommandEnabled("#films")
 
-//   myJson.results.forEach( (film) => {
-//     film.characters.forEach( (character) => {
-//       fetch(character)
-//       .then(function (response){
-//         return response.json()
-//       })
-//       .then(function (characterValue){
-//         console.log(characterValue.name)
-//       })
-//     })
-//     // console.log(film.title)
-//   })
-// })
-
-fetch("https://swapi.co/api/people/")
-.then(function(response){
-  return response.json();
-})
-.then(function(people){
-    people.results.forEach((results) => {
-      let ul = document.getElementById("characterName");
-      let li = document.createElement("li");
-      li.appendChild(document.createTextNode(results.name));
-      ul.appendChild(li);
-      // console.log(results.name)
-      // charName.textContent = results.name;
-    })
-})
+console.log(films);
