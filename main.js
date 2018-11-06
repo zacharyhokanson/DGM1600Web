8f72f66deb587cd17d3f07e78b17fbccfaa2bf1c
Zac Hokanson <zac.hokanson@gmail.com>
2018-11-06 10:47:33 -0700

    added some pokemon data

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-var charName = document.querySelector("#characterName");
-var hairColor = document.querySelector("#hairColor");
-var eyeColor = document.querySelector("#eyeColor");
+import {pokemon} from "/assets/pokemon.js";
 
-fetch('https://swapi.co/api/people/1/')
-.then(function(response) {
-  return response.json();
-})
-.then(function(myJson) {
-    charName.textContent = myJson.name;
-    hairColor.textContent = "Hair Color: " + myJson.hair_color;
-    eyeColor.textContent = "Eye Color: " + myJson.eye_color;
-  console.log(myJson.name);
-});
-//Test
+console.log(pokemon.length);
