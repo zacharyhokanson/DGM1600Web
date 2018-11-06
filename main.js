7cecbcf2cb1ee3844c1eac5522e70ba1909caf62
Zac Hokanson <zac.hokanson@gmail.com>
2018-11-05 18:13:16 -0700

    added senator display

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import {senators} from "/assets/senators.js";
 
+////////////////////////////////////////// VARIABLES //////////////////////////////////////////
+
 const senateData = senators.results[0].members;
 
 const senatorWithPics = senateData.map(senator => {
@@ -11,9 +13,11 @@ const fillStateMenu = senatorWithPics;
 
 const senatorPic = document.querySelector(".senatorPic");
 const stateMenu = document.getElementById("stateMenu");
+const senatorInfo = document.querySelector(".senatorInfo");
 
+////////////////////////////////////////// FUNCTIONS //////////////////////////////////////////
 
-fillStateMenu.forEach(senator => { //populates state dropdown menu
+fillStateMenu.forEach(senator => { //populates state dropdown menu, needs to be alphabetized and remove duplicates
   let optionTag = document.createElement("option");
   optionTag.setAttribute("id", senator.state);
   optionTag.textContent = senator.state;
@@ -22,19 +26,30 @@ fillStateMenu.forEach(senator => { //populates state dropdown menu
   // console.log(senator.state);
 });
 
-// function showSenator(senator, n){
-//     let imgItem = document.createElement("img");
-//     imgItem.src = senator[n].img;
-//     senatorPic.appendChild(imgItem);
-//     // console.log(senator[n].img);
-// }
+function displaySenator(senator){//takes senator object and displays information on webpage, needs to be able to display 2 senators
+    let imgItem = document.createElement("img");//need some way to clear info before adding new tags
+    let textItem = document.createElement("p");
+    imgItem.src = senator.img;
+    textItem.textContent = `${senator.first_name} ${senator.last_name} from ${senator.state}.`;//potentially create way to turn postal code into full name.
+    senatorPic.appendChild(imgItem);
+    senatorInfo.appendChild(textItem);
+}
 
-// showSenator(senatorWithPics, 3);
 
-function run(senator) {
-  let n = document.getElementById("stateMenu").value;
+function findSenator(senator) {//finds senator with matching state value to drop menu value, needs to be able to find multiple senators 
+  let stateValue = document.getElementById("stateMenu").value;
+  let senatorObject;
+  for(let i = 0; i < senator.length; i ++){
+    console.log(2);
+    if(stateValue == senator[i].state){
+      senatorObject = senator[i];
+      break;
+    }
+  }
+  displaySenator(senatorObject);//calls displaySenator function.
 };
 
+
 document.getElementById("stateMenu").addEventListener("change", function(){
-  run(senatorWithPics);
+  findSenator(senatorWithPics);
 });//Looks for change in stateMenu dropdown, look into how this works
