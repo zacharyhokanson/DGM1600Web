9b78ff086420fd44df69b2e7870a9c573d39a238
Zac Hokanson <zac.hokanson@gmail.com>
2018-11-15 00:15:10 -0700

    updated layout and create new card

--- gridcode.js
+++ gridcode.js
@@ -4,10 +4,50 @@ import {pokemon} from './assets/pokemon.js';
 ////////////////////////////////////////////VARIABLES////////////////////////////////////////////
 let pageWrapper = document.querySelector('.pageWrapper');
 
+let button = document.querySelector('#newCard')
+
+// let newCard = {
+//     "base": {
+//         "Attack": 49, 
+//         "Defense": 65, 
+//         "HP": 45, 
+//         "Sp.Atk": 49, 
+//         "Sp.Def": 65, 
+//         "Speed": 45
+//     }, 
+//     "cname": "\u83ca\u8349\u53f6", 
+//     "ename": "Chikorita", 
+//     "id": "152", 
+//     "jname": "\u30c1\u30b3\u30ea\u30fc\u30bf", 
+//     "skills": {}, 
+//     "type": [
+//         "\u8349"
+//     ]
+// }
+
+let newCard = new card();
+
 ////////////////////////////////////////////FUNCTIONS////////////////////////////////////////////
+function card() {
+    this.ename = 'Chikorita';
+    this.id = '152';
+    this.base = {
+        "Attack": Math.random(), 
+        "Defense": 65, 
+        "HP": 45, 
+        "Sp.Atk": 49, 
+        "Sp.Def": 65, 
+        "Speed": 45
+    }
+}
+
 pokemon.forEach(element  => {
     console.log(element.ename);
+    generateCard(element);
     
+});
+
+function generateCard(element){
     let img = document.createElement('img');
     let pokeName = document.createElement('p');
     let pokeStats = document.createElement('p');
@@ -33,8 +73,29 @@ pokemon.forEach(element  => {
     pokeName.textContent = element.ename;
     pokeName.className = 'has-text-centered';
 
-    pokeStats.textContent = `Attack: ${element.base.Attack}`;
+    pokeStats.textContent = `HP: ${element.base.HP} Attack: ${element.base.Attack} Sp. Atk: ${element.base["Sp.Atk"]}`;
     pokeStats.className = 'has-text-centered';
+};
 
-    
+
+
+
+
+button.addEventListener('click', () => {
+    console.log(`Thanks for clicking`);
+    pokemon.push(newCard);
+    console.log(pokemon);
+    generateCard(newCard);
+
 });
+
+
+// function Bird() {
+//     this.name = "Albert";
+//     this.color = "blue";
+//     this.numLegs = 2;
+//     // "this" inside the constructor always refers to the object being created
+//   }
+  
+//   let blueBird = new Bird();
+//   console.log(blueBird);
--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 ////////////////////////////////////////////VARIABLES////////////////////////////////////////////
-var charName = document.querySelector("#characterName");
-var hairColor = document.querySelector("#hairColor");
-var eyeColor = document.querySelector("#eyeColor");
+
+// var charName = document.querySelector("#characterName");
+// var hairColor = document.querySelector("#hairColor");
+// var eyeColor = document.querySelector("#eyeColor");
 
 ////////////////////////////////////////////FUNCTIONS////////////////////////////////////////////
-fetch('https://swapi.co/api/people/1/')
-.then(function(response) {
-  return response.json();
-})
-.then(function(myJson) {
-    charName.textContent = myJson.name;
-    hairColor.textContent = "Hair Color: " + myJson.hair_color;
-    eyeColor.textContent = "Eye Color: " + myJson.eye_color;
-  console.log(myJson.name);
-});
+
+// fetch('https://swapi.co/api/people/1/')
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(myJson) {
+//     charName.textContent = myJson.name;
+//     hairColor.textContent = "Hair Color: " + myJson.hair_color;
+//     eyeColor.textContent = "Eye Color: " + myJson.eye_color;
+//   console.log(myJson.name);
+// });
